0334d9813213236b1a5143c39f7b85a4b1b9a636
magix257 <55533437+magix257@users.noreply.github.com>
2019-10-29 10:03:57 +0100

    Model and View
    Wywalamy sesje oraz zwracanie strony w postaci Stringa wszystko obsluguje obiekt Model and View

--- src/main/java/com/telusko/springmvcboot/HomeController.java
+++ src/main/java/com/telusko/springmvcboot/HomeController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController 
@@ -16,15 +17,14 @@ public class HomeController
 	}
 	
 	@RequestMapping("add")
-	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, HttpSession session)
+	public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2") int j)
 	{
-	
-		
+	ModelAndView mv = new ModelAndView();
+	mv.setViewName("result.jsp");
 		int num3 = i + j;
-	
-		session.setAttribute("num3", num3);
+	mv.addObject("num3",num3);
 		
-		return "result.jsp";
+		return mv;
 	}
 	
 }
