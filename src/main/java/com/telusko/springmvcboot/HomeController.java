0e7de51e442d0a83facefaf3504a22ace0d90d96
magix257 <55533437+magix257@users.noreply.github.com>
2019-10-29 09:55:27 +0100

    dodawanie po staremu

--- src/main/java/com/telusko/springmvcboot/HomeController.java
+++ src/main/java/com/telusko/springmvcboot/HomeController.java
@@ -1,10 +1,10 @@
 package com.telusko.springmvcboot;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController 
@@ -16,13 +16,12 @@ public class HomeController
 	}
 	
 	@RequestMapping("add")
-	public String add(HttpServletRequest req)
+	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, HttpSession session)
 	{
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
+	
 		
 		int num3 = i + j;
-		HttpSession session = req.getSession();
+	
 		session.setAttribute("num3", num3);
 		
 		return "result.jsp";
